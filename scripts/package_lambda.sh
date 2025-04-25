f06b3b66e49de534717f41519758b0a3d62842cf
nathanEV <nathan@exponentialview.co>
2025-04-25 17:03:47 +0200

    Improve lambda packaging script: install requirements into build/python and flatten dependencies to build root

--- scripts/package_lambda.sh
+++ scripts/package_lambda.sh
@@ -4,12 +4,18 @@ set -e
 # 1) Prep build area
 cd lambda_package
 rm -rf build
-mkdir -p build/python
+mkdir -p build
 mkdir -p build/kane_lambda
 
 # 2) Install all requirements into build/python
 git_root=$(dirname "$BASH_SOURCE")
 python3 -m pip install -r ../requirements.txt --target build/python --upgrade
+
+# If dependencies installed into a python/ dir, move them to root
+if [ -d build/python ]; then
+  mv build/python/* build/
+  rm -rf build/python
+fi
 
 # 3) Copy our code & creds
 cp -r ../kane_lambda/* build/kane_lambda/
